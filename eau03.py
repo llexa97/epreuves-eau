f51ef50f2749186cee95de5aa23068b041304c9d
unknown <dondinaxel@icloud.com>
2023-10-11 14:06:06 +0200

    Upload de l'exercice 4 (suite fibonacci)

---
+++ eau03.py
@@ -0,0 +1,39 @@
+import sys
+
+# création des variable initial
+args = sys.argv[1:]
+fibo_list = []
+
+
+# Fonction qui crée la liste fibonacci
+def create_fibo_list(valeur):
+    a = 0
+    b = 1
+    if len(fibo_list) < 2:
+        fibo_list.append(a)
+        fibo_list.append(b)
+        print(fibo_list)
+    for i in range(0, valeur - 1):
+        a = fibo_list[-2]
+        b = fibo_list[-1]
+
+        fibo_list.append(a + b)
+        print(fibo_list)
+    return fibo_list[valeur]
+
+
+# Gestion des erreur
+if not args[0].isdigit():
+    print(-1)
+    sys.exit()
+elif int(args[0]) <= 0:
+    print(-1)
+    sys.exit()
+
+
+# Valeur chercher :
+n = int(args[0])
+resutat = create_fibo_list(n)
+
+# Affichage du resultat
+print(resutat)
